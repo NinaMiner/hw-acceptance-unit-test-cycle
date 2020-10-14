e370fde071952a6bbe0ea3f0368e5fe0be3ab3c5
EC2 Default User <ec2-user@ip-172-31-15-195.ec2.internal>
2020-10-14 02:55:24 +0000

    Attempt at Rspec Testing

---
+++ rottenpotatoes/spec/movie_factory.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :movie do
+    title 'Null'
+    rating 'PG'
+    description 'No description'
+    release_date '2020-04-15'
+  end
+end
--- rottenpotatoes/spec/rails_helper.rb
+++ rottenpotatoes/spec/rails_helper.rb
@@ -7,7 +7,130 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
-# Add additional requires below this line. Rails is not loaded until this point!
+
+
+describe MoviesController do
+  describe 'Search movies by same director' do
+    it 'should call Movie.search' do
+      expect(Movie).to recieve(:search).with('Aladdin')
+      get :search, {title: 'Aladdin'}
+    end
+
+  it 'should assign similiar movies if director exists' do
+    movies = ['Hanna', 'Harry Potter']
+    Movie.stub(:search).with('Hanna').and_return(movies)
+    get :search, { title: 'Hanna'}
+    expect(assigns(:search)).to eql(movies)
+  end
+
+  it 'should redirect to home page if no director is listed' do
+    Movie.stub(:search).with('Null').and_return(nil)
+    get :search, { title: 'Null'}
+    expect(response).to redirect_to(root_url)
+  end
+end
+
+  describe 'GET index' do
+    let!(:movie) {FactoryGirl.create(:movie)}
+
+    it 'should render the index template' do
+      get :index
+      expect(response).to render_template('index')
+    end
+
+    it 'should assign instance variable for title header' do
+      get :index, { sort: 'title'}
+      expect(assigns(:title_header)).to eql('hilite')
+    end
+
+    it 'should assign instance variable for release_date header' do
+      get :index, { sort: 'release_date'}
+      expect(assigns(:date_header)).to eql('hilite')
+    end
+  end
+
+  describe 'GET new' do
+    let!(:movie) { Movie.new }
+
+    it 'should render the new template' do
+      get :new
+      expect(response).to render_template('new')
+    end
+  end
+
+  describe 'POST #create' do
+    it 'creates a new movie' do
+      expect {post :create, movie: FactoryGirl.attributes_for(:movie)
+      }.to change { Movie.count }.by(1)
+    end
+
+    it 'redirects to the movie index page' do
+      post :create, movie: FactoryGirl.attributes_for(:movie)
+      expect(response).to redirect_to(movies_url)
+    end
+  end
+
+  describe 'GET #show' do
+    let!(:movie) { FactoryGirl.create(:movie) }
+    before(:each) do
+      get :show, id: movie.id
+    end
+
+    it 'should find the movie' do
+      expect(assigns(:movie)).to eql(movie)
+    end
+
+    it 'should render the show template' do
+      expect(response).to render_template('show')
+    end
+  end
+
+  describe 'GET #edit' do
+    let!(:movie) { FactoryGirl.create(:movie) }
+    before do
+      get :edit, id: movie.id
+    end
+
+    it 'should find the movie' do
+      expect(assigns(:movie)).to eql(movie)
+    end
+
+    it 'should render the edit template' do
+      expect(response).to render_template('edit')
+    end
+  end
+
+  describe 'PUT #update' do
+    let(:movie1) { FactoryGirl.create(:movie) }
+    before(:each) do
+      put :update, id: movie1.id, movie: FactoryGirl.attributes_for(:movie, title: 'Modified')
+    end
+
+    it 'updates an existing movie' do
+      movie1.reload
+      expect(movie1.title).to eql('Modified')
+    end
+
+    it 'redirects to the movie page' do
+      expect(response).to redirect_to(movie_path(movie1))
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    let!(:movie1) { FactoryGirl.create(:movie) }
+
+    it 'destroys a movie' do
+      expect { delete :destroy, id: movie1.id
+      }.to change(Movie, :count).by(-1)
+    end
+
+    it 'redirects to movies#index after destroy' do
+      delete :destroy, id: movie1.id
+      expect(response).to redirect_to(movies_path)
+    end
+  end
+end
+
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
